5e0db5b9f537422a865ab6f0a14b4372c40206b3
abhii6700 <abhishekyadav6700.ay@gmail.com>
2021-07-05 17:47:20 +0530

    deployed on heroku

--- db/db.sql
+++ db/db.sql
@@ -7,12 +7,7 @@
 -- list all tables \d
 
 --RESTAURANT TABLE
-CREATE TABLE restaurants(
-    id BIGSERIAL NOT NULL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    location VARCHAR(50) NOT NULL,
-    price_range INT NOT NULL check(price_range >=1 and price_range <=5)
-);
+CREATE TABLE restaurants(id BIGSERIAL NOT NULL PRIMARY KEY,name VARCHAR(50) NOT NULL,location VARCHAR(50) NOT NULL,price_range INT NOT NULL check(price_range >=1 and price_range <=5));
 
 
 -- REVIW AND RATING TABLE
